ab76cc840861e0892b2f888fee84454f2a0cb59d
alanatilio <alanarapa15@gmail.com>
2023-05-29 16:44:12 -0500

    prueva

--- db/dbUsuarios.php
+++ db/dbUsuarios.php
@@ -7,81 +7,98 @@ class dbUsuarios
      *****************************************************************************/
     public function loginUsuario($email)
     {
-        global $dbConexion, $conexion;
+        global $conexion;
 
-        // $conexion = $dbs->obtenerConexion();
-
-        $consultaEspecialista = "SELECT L.idUsuario, E.Nombre, E.Apellido, L.Email, 0 AS Rol 
-                    From login AS L
+        $consulta = "SELECT L.idUsuario, E.Nombre, E.Apellido, L.Email, 0 AS Rol 
+                    FROM login AS L
                     INNER JOIN especialista AS E ON L.idEspecialista = E.id
-                    WHERE Email = '$email' AND Estado = 1";
-
-        $consultaAdministrador = "SELECT L.idUsuario, B.Nombre, B.Apellido, B.Cargo, L.Email, 1 AS Rol 
-                    From login AS L
+                    WHERE Email = ? AND Estado = 1
+                    UNION ALL 
+                    SELECT L.idUsuario, B.Nombre, B.Apellido, L.Email, 1 AS Rol 
+                    FROM login AS L
                     INNER JOIN administrador AS B ON L.idAdministrador = B.id
-                    WHERE Email = '$email' AND Estado = 1";
-        $resultadoEspecialista = mysqli_query($conexion, $consultaEspecialista);
-        $resultadoAdministrador = mysqli_query($conexion, $consultaAdministrador);
-        if (mysqli_num_rows($resultadoEspecialista) > 0) {
-            return mysqli_fetch_assoc($resultadoEspecialista);
-        } else if (mysqli_num_rows($resultadoAdministrador) > 0) {
-            return mysqli_fetch_assoc($resultadoAdministrador);
-        } else {
-            return false;
+                    WHERE Email = ? AND Estado = 1";
+
+        $stmt = mysqli_prepare($conexion, $consulta);
+
+        // Verificar si la preparación de la consulta fue exitosa
+        if ($stmt) {
+            // Vincular los parámetros a la consulta preparada
+            mysqli_stmt_bind_param($stmt, "ss", $email, $email);
+
+            // Ejecutar la consulta preparada
+            mysqli_stmt_execute($stmt);
+
+            // Obtener el resultado de la consulta
+            $resultado = mysqli_stmt_get_result($stmt);
+
+            if ($resultado && mysqli_num_rows($resultado) > 0) {
+                return mysqli_fetch_assoc($resultado);
+            }
         }
 
-        $dbConexion->close();
+        return false;
     }
+
 
     /******************************************************************************/
     /*                     VERIFICAR SI LA CONTRASEÑA EN LA MISMA                 */
     /******************************************************************************/
     public function verificarPass($email, $pass)
     {
-        global $dbConexion, $conexion;
+        global $conexion;
 
         $email = trim(strtolower($email));
 
-        $consulta = "SELECT Email, Pass FROM login
-                WHERE Email = '$email'";
+        $consulta = "SELECT Pass FROM login WHERE Email = ?";
+        $stmt = mysqli_prepare($conexion, $consulta);
 
-        $resultado = mysqli_query($conexion, $consulta);
+        if ($stmt) {
+            mysqli_stmt_bind_param($stmt, "s", $email);
+            mysqli_stmt_execute($stmt);
+            $resultado = mysqli_stmt_get_result($stmt);
 
-        if (mysqli_num_rows($resultado) > 0) {
-            $resul = mysqli_fetch_assoc($resultado);
-            if ($email == $resul["Email"] && password_verify($pass, $resul["Pass"])) {
-                return true;
-            } else {
-                return false;
+            if ($resultado && mysqli_num_rows($resultado) > 0) {
+                $resul = mysqli_fetch_assoc($resultado);
+                if (password_verify($pass, $resul["Pass"])) {
+                    return true;
+                }
             }
         }
-        $dbConexion->close();
+
+        return false;
     }
+
 
     /******************************************************************************/
     /*        VERIFICAR SI LA CONTRASEÑA EN LA MISMA SEGUN AL ID Y EMAIL          */
     /******************************************************************************/
     public function validarPassReset($id, $email, $oldPass)
     {
-        global $dbConexion, $conexion;
+        global $conexion;
 
         $email = trim(strtolower($email));
 
         $consulta = "SELECT Email, Pass FROM login
-                WHERE idUsuario = '$id' AND Email = '$email'";
+                WHERE idUsuario = ? AND Email = ?";
+        $stmt = mysqli_prepare($conexion, $consulta);
 
-        $resultado = mysqli_query($conexion, $consulta);
+        if ($stmt) {
+            mysqli_stmt_bind_param($stmt, "is", $id, $email);
+            mysqli_stmt_execute($stmt);
+            $resultado = mysqli_stmt_get_result($stmt);
 
-        if (mysqli_num_rows($resultado) > 0) {
-            $resul = mysqli_fetch_assoc($resultado);
-            if ($email == $resul["Email"] && password_verify($oldPass, $resul["Pass"])) {
-                return true;
-            } else {
-                return false;
+            if ($resultado && mysqli_num_rows($resultado) > 0) {
+                $resul = mysqli_fetch_assoc($resultado);
+                if (password_verify($oldPass, $resul["Pass"])) {
+                    return true;
+                }
             }
         }
-        $dbConexion->close();
+
+        return false;
     }
+
 
 
     /*****************************************************************************
@@ -89,30 +106,35 @@ class dbUsuarios
      *****************************************************************************/
     public function SelectUsuario($id)
     {
-        global $dbConexion, $conexion;
+        global $conexion;
 
-        //EL IFNULL debuelve un resulataso si es que uno de ellos es nulo o vacio
         $consulta = "SELECT L.Rol, L.Email, L.Pass, E.DNI,
-                    IFNULL (B.id, E.id) AS IdCliente,
-                    IFNULL(B.Nombre, E.Nombre) AS Nombre, 
-                    IFNULL(B.Apellido, E.Apellido) AS Apellido, 
-                    IFNULL(B.Cargo, E.Cargo) AS Cargo, 
-                    IFNULL(B.direccion, E.direccion) AS Direccion, 
-                    IFNULL(B.telefono, E.telefono) AS Telefono 
-                FROM login AS L 
-                LEFT JOIN especialista AS E ON L.idEspecialista = E.id 
-                LEFT JOIN administrador AS B ON L.idAdministrador = B.id 
-                WHERE L.idUsuario = $id";
+                IFNULL (B.id, E.id) AS IdCliente,
+                IFNULL(B.Nombre, E.Nombre) AS Nombre, 
+                IFNULL(B.Apellido, E.Apellido) AS Apellido, 
+                IFNULL(B.Cargo, E.Cargo) AS Cargo, 
+                IFNULL(B.direccion, E.direccion) AS Direccion, 
+                IFNULL(B.telefono, E.telefono) AS Telefono 
+            FROM login AS L 
+            LEFT JOIN especialista AS E ON L.idEspecialista = E.id 
+            LEFT JOIN administrador AS B ON L.idAdministrador = B.id 
+            WHERE L.idUsuario = ?";
 
-        $resultado = mysqli_query($conexion, $consulta);
+        $stmt = mysqli_prepare($conexion, $consulta);
 
-        if (mysqli_num_rows($resultado) > 0) {
-            return mysqli_fetch_assoc($resultado); //Recupera una fila de resultados como un array asociativo
-        } else {
-            return [];
+        if ($stmt) {
+            mysqli_stmt_bind_param($stmt, "i", $id);
+            mysqli_stmt_execute($stmt);
+            $resultado = mysqli_stmt_get_result($stmt);
+
+            if ($resultado && mysqli_num_rows($resultado) > 0) {
+                return mysqli_fetch_assoc($resultado);
+            }
         }
-        $dbConexion->close();
+
+        return [];
     }
+
 
 
     /*****************************************************************************
@@ -120,66 +142,58 @@ class dbUsuarios
      *****************************************************************************/
     public function insertUsuario($rol, $usuario)
     {
+        global $conexion;
 
-        global $dbConexion, $conexion;
+        $nombre = trim($usuario['Nombre']);
+        $apellido = trim($usuario['Apellido']);
+        $direccion = trim($usuario['Direccion']);
+        $telefono = trim($usuario['Telefono']);
+        $email = trim(strtolower($usuario['Email']));
+        $pass = trim($usuario['Pass']);
+        $passHash = password_hash($pass, PASSWORD_BCRYPT, ["cost" => 11]);
+        $cargo = trim($usuario['Cargo']);
 
-        if ($rol == 1) { //TRIM()permite eliminar caracteres o espacios específicos del principio,del final o de ambos extremos de una cadena
-            $nombre = trim($usuario['Nombre']);
-            $apellido = trim($usuario['Apellido']);
-            $direccion = trim($usuario['Direccion']);
-            $telefono = trim($usuario['Telefono']);
-            $cargo = trim($usuario['Cargo']);
-            $email = trim(strtolower($usuario['Email']));
-            $pass = trim($usuario['Pass']);
+        $consulta = "";
+        if ($rol == 1) {
 
-            //encriptar la contraseña - cost es el nivel de encriptamiento de la misma
-            $passHash = password_hash($pass, PASSWORD_BCRYPT, ["cost" => 11]);
-
-            $consulta = "INSERT INTO administrador (Nombre, Apellido, Cargo, Direccion, Telefono )
-        VALUES ('$nombre', '$apellido', '$cargo', '$direccion', '$telefono' );";
-
-            mysqli_query($conexion, $consulta);
-
-            $ultimo_id = mysqli_insert_id($conexion);
-
-            $fechaHoraActual = date('Y-m-d H:i:s');
-
-            $consulta = "INSERT INTO login (Rol, idAdministrador, Email, Pass, Activacion, Estado )
-            VALUES ( $rol, $ultimo_id, '$email', '$passHash','$fechaHoraActual', 1 );";
-
-            mysqli_query($conexion, $consulta);
+            $consulta = "INSERT INTO administrador (Nombre, Apellido, Cargo, Direccion, Telefono)
+                    VALUES (?, ?, ?, ?, ?)";
         } else if ($rol == 0) {
             $dni = trim($usuario['DNI']);
-            $nombre = trim($usuario['Nombre']);
-            $apellido = trim($usuario['Apellido']);
-            $cargo = trim($usuario['Cargo']);
-            $direccion = trim($usuario['Direccion']);
-            $telefono = trim($usuario['Telefono']);
-            $email = trim(strtolower($usuario['Email'])); // strtolower : Poner una cadena en minúsculas
-            $pass = trim($usuario['Pass']);
-
-            //encriptar la contraseña - cost es el nivel de encriptamiento de la misma
-            $passHash = password_hash($pass, PASSWORD_BCRYPT, ["cost" => 11]);
-
-            $consulta = "INSERT INTO especialista (DNI, Nombre, Apellido, Cargo, Direccion, Telefono )
-        VALUES ('$dni',  '$nombre', '$apellido', '$cargo', '$direccion', '$telefono' );";
-
-            mysqli_query($conexion, $consulta);
-
-            $ultimo_id = mysqli_insert_id($conexion);
-
-            $fechaHoraActual = date('Y-m-d H:i:s');
-
-            $consulta = "INSERT INTO login (Rol, idAdministrador, Email, Pass, Activacion, Estado )
-            VALUES ( $rol, $ultimo_id, '$email', '$passHash','$fechaHoraActual', 1 );";
-
-            mysqli_query($conexion, $consulta);
+            $consulta = "INSERT INTO especialista (DNI, Nombre, Apellido, Cargo, Direccion, Telefono)
+                    VALUES (?, ?, ?, ?, ?, ?)";
         }
 
-        return mysqli_affected_rows($conexion);
+        if (!empty($consulta)) {
+            $stmt = mysqli_prepare($conexion, $consulta);
+            if ($stmt) {
+                if ($rol == 1) {
+                    mysqli_stmt_bind_param($stmt, "sssss", $nombre, $apellido, $cargo, $direccion, $telefono);
+                } else if ($rol == 0) {
+                    mysqli_stmt_bind_param($stmt, "ssssss", $dni, $nombre, $apellido, $cargo, $direccion, $telefono);
+                }
+                mysqli_stmt_execute($stmt);
+                mysqli_stmt_close($stmt);
 
-        // $dbConexion->close();
+                $ultimo_id = mysqli_insert_id($conexion);
+                $fechaHoraActual = date('Y-m-d H:i:s');
+
+                $consultaLogin = "INSERT INTO login (Rol, idAdministrador, Email, Pass, Activacion, Estado)
+                            VALUES (?, ?, ?, ?, ?, 1)";
+                $stmtLogin = mysqli_prepare($conexion, $consultaLogin);
+                if ($stmtLogin) {
+                    mysqli_stmt_bind_param($stmtLogin, "iisss", $rol, $ultimo_id, $email, $passHash, $fechaHoraActual);
+                    mysqli_stmt_execute($stmtLogin);
+                    mysqli_stmt_close($stmtLogin);
+                }
+
+                return mysqli_affected_rows($conexion);
+            }
+        }
+
+        return false;
     }
+
 
     /*****************************************************************************
      *                ACTUALIZO EL USUARIO EN LA TABLA SEGUN EL ID
