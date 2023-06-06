84bda3ae4787a60eb53fbc06f2de319a0dd6db30
PatrickCaipa <pcaipasalas123@gmail.com>
2023-06-06 16:54:17 -0500

    Logica de Empresas

--- views/modules/empresa.php
+++ views/modules/empresa.php
@@ -9,41 +9,63 @@
         <div class="main">
             <div class="formm">
                     <?php
-                    // Conexión a la base de datos
-                    $servername = "localhost";
-                    $username = "root";
-                    $password = "";
-                    $database = "da";
-                    $conexion = new mysqli($servername, $username, $password, $database);
-
-                    // Verificar la conexión
-                    if ($conexion->connect_error) {
-                        die("Error de conexión: " . $conexion->connect_error);
-                    }
-                    //ACA estan los cambioooooos
+                    $action = $_GET["action"];             
+                    //====================================================================
+                    //==            Mostrar los datos dentro del formulario             ==
+                    //====================================================================
                     if ($_SERVER["REQUEST_METHOD"] == "GET") {
                         // Aca se deben procesar los datos del formulario ejecutado
-                        $id = $_GET["id"];
-                        $consulta = "SELECT * FROM empresa WHERE idEmpresa = $id";
-                        $resultado=$conexion->query($consulta);
+                        $id = isset($_GET["id"]) ? $_GET["id"] : "";
+                        if ($action == "update" || $action == "delete") {
+                            $consulta = "SELECT * FROM empresa WHERE idEmpresa = $id";
+                            $resultado = $conexion->query($consulta);
+                            if ($resultado->num_rows > 0) {
+                                $row = $resultado->fetch_assoc();
+                                $idEmpresa = $row["idEmpresa"];
+                                $nombreEmpresa = $row["nombreEmpresa"];
+                                $ruc = $row["ruc"];
+                                $telefono = $row["telefono"];
+                                $email = $row["email"];
+                                $num_partida = $row["numeroPartida"];
+                                $mipe = $row["mipe"];
+                            }
+                        } else if ($action == "add") {
+                            // Inicializar los valores en blanco para un nuevo registro
+                            $id="";
+                            $idEmpresa = "";
+                            $nombreEmpresa = "";
+                            $ruc = "";
+                            $telefono = "";
+                            $email = "";
+                            $num_partida = "";
+                            $mipe = "";
+                        }
+                    }
+                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                        // Obtener los valores del formulario
+                        $id = $_POST["id"];
+                        $nombreEmpresa = $_POST["Nombre"];
+                        $ruc = $_POST["ruc"];
+                        $telefono = $_POST["telefono"];
+                        $email = $_POST["email"];
+                        $num_partida = $_POST["num_partida"];
+                        $mipe = $_POST["mipe"];
+                
+                        //Verficar action
+                        if ($action == "update") {
+                            
+                        } else if ($action == "add") {
+                            
+                        } else if ($action == "delete") {
 
-                        if ($resultado->num_rows > 0) {
-                            $row = $resultado->fetch_assoc();
-                            $idEmpresa = $row["idEmpresa"];
-                            $nombreEmpresa = $row["nombreEmpresa"];
-                            $ruc = $row["ruc"];
-                            $telefono = $row["telefono"];
-                            $email = $row["email"];
-                            $num_partida= $row["numeroPartida"];
-                            $mipe = $row["mipe"];
                         }
                     }
                     ?>
 
-                <form action="?m=panel&mod=categoria&action=" method="POST" enctype="multipart/form-data">
+                <form action="?m=panel&mod=empresa&action=" method="POST" enctype="multipart/form-data">
                     <input type="hidden" name="id" value="">
                     <span> Id </span>
-                    <input id="noEdid" title="No se puede modificar" disabled required type="text" name="Nombre" value="<?php echo $id; ?>">
+                    <input id="noEdid" title="No se puede modificar" disabled required type="text" name="Nombre" value="<?php echo $idEmpresa   ; ?>">
                     <span> Nombre Empresa</span>
                     <input required type="text" name="Nombre" value="<?php echo $nombreEmpresa; ?>">
                     <span> RUC </span>
@@ -57,7 +79,17 @@
                     <span> MIPE </span>
                     <input required type="number" min="0" name="mipe" value="<?php echo $mipe; ?>" >
                     <br><br>
-                    <button type="submit" name="action" id="ac" style="color:red;" class="form_login" value="Agregar">Agregar</button>
+                    <button type="submit" name="action" id="ac" style="color:red;" class="form_login" value="">
+                    <?php
+                    if ($action == "update") {
+                        echo "Actualizar";
+                    } else if ($action == "add") {
+                        echo "Agregar";
+                    } else if ($action == "delete") {
+                        echo "Eliminar";
+                    }
+                    ?>
+                    </button>
                     
                 </form>
             </div>
