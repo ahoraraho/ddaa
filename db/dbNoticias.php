659f6192a8726005b46a21e20d3e055adee6695a
PatrickCaipa <pcaipasalas123@gmail.com>
2023-07-03 17:26:46 -0500

    Funcionamiento de Noticias - ajustar mensajes

--- db/dbNoticias.php
+++ db/dbNoticias.php
@@ -23,15 +23,15 @@ class dbNoticias
     }
 
     // Seleccionar registro de la tabla noticias
-    public function selectNoticia()
+    public function selectNoticia($idNoticia)
     {
         global $conexion;
 
-        $consulta = "SELECT * FROM noticias WHERE destacado = 1";
+        $consulta = "SELECT * FROM noticias WHERE idNoticia = ?";
         $stmt = mysqli_prepare($conexion, $consulta);
 
         if ($stmt) {
-            // mysqli_stmt_bind_param($stmt); // Mantener "i" para un entero
+            mysqli_stmt_bind_param($stmt, "i", $idNoticia);
             mysqli_stmt_execute($stmt);
             $resultado = mysqli_stmt_get_result($stmt);
 
@@ -41,5 +41,63 @@ class dbNoticias
         }
 
         return [];
+    }
+
+    public function insertNoticia($titulo, $descripcion, $fecha, $destacado, $imagen)
+    {
+        global $conexion;
+
+        $consulta = "INSERT INTO noticias (titulo, descripcion, fecha, destacado, imagen)
+                    VALUES ('$titulo', '$descripcion', '$fecha', '$destacado','$imagen')";
+
+        mysqli_query($conexion, $consulta);
+        return mysqli_affected_rows($conexion);
+    }
+
+    public function updateNoticia($idNoticia, $titulo, $descripcion, $fecha, $destacado,$imagen)
+    {
+        global $conexion;
+
+        $consulta = "UPDATE noticias
+                    SET 
+                    titulo = '$titulo',
+                    descripcion = '$descripcion',
+                    fecha = '$fecha',
+                    destacado = $destacado,
+                    imagen = '$imagen'
+                    WHERE idNoticia = $idNoticia";
+
+        mysqli_query($conexion, $consulta);
+
+        $consulta_destacado = "UPDATE noticias
+                    SET
+                    destacado = 0
+                    WHERE idNoticia != $idNoticia";
+
+        mysqli_query($conexion, $consulta_destacado);
+        return mysqli_affected_rows($conexion);
+    }
+
+    public function deleteNoticia($idNoticia)
+    {
+        global $conexion;
+
+        $consulta = "DELETE FROM noticias WHERE idNoticia = $idNoticia";
+        mysqli_query($conexion, $consulta);
+    }
+
+    public function MayorIdNoticia()
+    {
+        global $conexion;
+
+        $consulta = "SELECT MAX(idNoticia) as maxId FROM noticias";
+
+            $resultado = mysqli_query($conexion, $consulta);
+
+            if ($resultado && mysqli_num_rows($resultado) > 0) {
+                return mysqli_fetch_all($resultado, MYSQLI_ASSOC);
+            } else {
+                return [];
+            }
     }
 }
